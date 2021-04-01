c12a24bb158fbc741d5db954c4dfbc06e20a8424
lourlm <l.lozano.2017@alumnos.urjc.es>
2021-04-01 21:01:13 +0200

    estados del boton

--- P2/index.js
+++ P2/index.js
@@ -4,13 +4,15 @@ display = document.getElementById("display")
 suma = document.getElementById("suma")
 igual = document.getElementById("igual")
 clear = document.getElementById("clear")
+option = document.getElementById("option")
 
 //-- Estados de la calculadora
 const ESTADO = {
     INIT: 0,
     OP1: 1,
-    OPERATION: 2,
-    OP2: 3
+    OP2: 2,
+    OP3: 3,
+    OP4: 4
 }
  
  //-- Variable de estado de la calculadora
@@ -18,23 +20,21 @@ const ESTADO = {
  let estado = ESTADO.INIT;   
 
 //-- Función de retrollamada de los digitos
-function digito(ev)
-{
-  
-    if (estado == ESTADO.INIT) {
-
-        display.innerHTML = ev.target.value;
-
-        //-- Pasar al siguiente estado
+function digito(bottom){
+    if(estado == ESTADO.INIT){
+        display.innerHTML = bottom;
         estado = ESTADO.OP1;
-
-    } else {
-       
-        //--En cualquier otro estado lo añadimos
-        display.innerHTML += ev.target.value;
-
-    } 
-    
+    }else if(estado == ESTADO.OP1){
+        display.innerHTML += bottom;
+    }else if(estado == ESTADO.OP2){
+        display.innerHTML += bottom;
+      estado = ESTADO.OP3;
+    }else if (estado == ESTADO.OP3) {
+      display.innerHTML +=  bottom;
+      estado = ESTADO.OP4;
+    }else if (estado == ESTADO.OP4){
+      display.innerHTML += bottom;
+    }
 }
 
 //-- Obtener una colección con todos los elementos
