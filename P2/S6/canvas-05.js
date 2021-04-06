115d8225011a5790002df9dd963fcff99b8fc0d2
lourlm <l.lozano.2017@alumnos.urjc.es>
2021-04-06 09:52:24 +0200

    mas jemplos

---
+++ P2/S6/canvas-05.js
@@ -0,0 +1,24 @@
+console.log("Ejecutando JS...");
+
+const canvas = document.getElementById("canvas");
+
+//-- Definir el tamaño del convas
+canvas.width = 200;
+canvas.height = 100;
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+
+ctx.beginPath();
+    ctx.arc(100,50,20,0, Math.PI/2);
+    ctx.strokeStyle = 'purple';
+    ctx.lineWidth = 3;
+    ctx.fillStyle = 'yellow'
+    
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+    ctx.fillStyle()
+    
+ctx.closePath()
