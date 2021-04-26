6560b8cba4f1b33213a07620535aa0b74c13bc5f
lourlm <l.lozano.2017@alumnos.urjc.es>
2021-04-26 19:49:14 +0200

    creacion de bola

--- P3/index.js
+++ P3/index.js
@@ -9,12 +9,25 @@ canvas.height = 900;
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
 
+// Mi bloque
 ctx.beginPath();
-ctx.rect(225,840, 80, 20);
-//-- Color de relleno del rectángulo
-ctx.fillStyle = 'white';
-//-- Mostrar el relleno
-ctx.fill();
-//-- Mostrar el trazo del rectángulo
-ctx.stroke();
+    ctx.rect(210,840, 80, 20);
+    //-- Color de relleno del rectángulo
+    ctx.fillStyle = 'white';
+    //-- Mostrar el relleno
+    ctx.fill();
+    //-- Mostrar el trazo del rectángulo
+    ctx.stroke();
+ctx.closePath();
+
+// Mi bola
+ctx.beginPath();
+    ctx.arc(250, 680, 10, 0, 2 * Math.PI);
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 3;
+    ctx.fillStyle = 'lightblue';
+    //-- Dibujar el trazo
+    ctx.stroke()
+    //-- Dibujar el relleno
+    ctx.fill()
 ctx.closePath();
