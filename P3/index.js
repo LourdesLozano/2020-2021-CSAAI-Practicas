2c7cb140010b2e28457a103b3c8c60ff27324f34
lourlm <l.lozano.2017@alumnos.urjc.es>
2021-04-26 20:08:54 +0200

    animacion bola

--- P3/index.js
+++ P3/index.js
@@ -6,28 +6,71 @@ const canvas = document.getElementById("canvas");
 canvas.width = 500;
 canvas.height = 900;
 
+// Coordenadas
+let X_bloque = 210;
+let Y_bloque = 840;
+let X_bola = 250;
+let Y_bola = 680;
+
+// Velocidades
+let velocidad_X = 4;
+let velocidad_Y = 2;
+
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
 
-// Mi bloque
-ctx.beginPath();
-    ctx.rect(210,840, 80, 20);
-    //-- Color de relleno del rectángulo
-    ctx.fillStyle = 'white';
-    //-- Mostrar el relleno
-    ctx.fill();
-    //-- Mostrar el trazo del rectángulo
-    ctx.stroke();
-ctx.closePath();
+function update(){
 
-// Mi bola
-ctx.beginPath();
-    ctx.arc(250, 680, 10, 0, 2 * Math.PI);
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 3;
-    ctx.fillStyle = 'lightblue';
-    //-- Dibujar el trazo
-    ctx.stroke()
-    //-- Dibujar el relleno
-    ctx.fill()
-ctx.closePath();
+    console.log("test");
+
+    // Rebote vertical
+    if (X_bola < 12 || X_bola >= (canvas.width - 10) ) {
+        velocidad_X = -velocidad_X;
+    }
+
+    // Rebote horizontal
+    if (Y_bola <= 12 || Y_bola > (canvas.height - 12)) {
+        velocidad_Y = -velocidad_Y;
+    }
+
+    // Actualizamos posicion
+    X_bola = X_bola + velocidad_X;
+    Y_bola = Y_bola + velocidad_Y;
+
+    // Borrar el canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Dibujamos elementos visibles
+
+    // Mi bloque
+    ctx.beginPath();
+        ctx.rect(X_bloque,Y_bloque, 80, 20);
+        //-- Color de relleno del rectángulo
+        ctx.fillStyle = 'white';
+        //-- Mostrar el relleno
+        ctx.fill();
+        //-- Mostrar el trazo del rectángulo
+        ctx.stroke();
+    ctx.closePath();
+
+    // Mi bola
+    ctx.beginPath();
+        ctx.arc(X_bola, Y_bola, 10, 0, 2 * Math.PI);
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 3;
+        ctx.fillStyle = 'lightblue';
+        //-- Dibujar el trazo
+        ctx.stroke()
+        //-- Dibujar el relleno
+        ctx.fill()
+    ctx.closePath();
+
+    // Volver a ejecutar cuando toque
+    requestAnimationFrame(update);
+}
+
+// Empezamos funciton
+update();
+
+    
+
