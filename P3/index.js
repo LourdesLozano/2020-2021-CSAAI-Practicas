f741464292fbbd83226fe9827622508edc97f1ec
lourlm <l.lozano.2017@alumnos.urjc.es>
2021-04-26 19:39:08 +0200

    creacion bloque

--- P3/index.js
+++ P3/index.js
@@ -5,3 +5,16 @@ const canvas = document.getElementById("canvas");
 //-- Definir el tamaño del convas
 canvas.width = 500;
 canvas.height = 900;
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(225,840, 80, 20);
+//-- Color de relleno del rectángulo
+ctx.fillStyle = 'white';
+//-- Mostrar el relleno
+ctx.fill();
+//-- Mostrar el trazo del rectángulo
+ctx.stroke();
+ctx.closePath();
