5b25da487b97c7cf8a404d2c4dd833803f3a6fa7
lourlm <l.lozano.2017@alumnos.urjc.es>
2021-05-06 18:37:46 +0200

    poniendp boton de grises

--- P4/index.js
+++ P4/index.js
@@ -4,17 +4,34 @@ const canvas = document.getElementById("canvas");
 const img = document.getElementById('original')
 const ctx = canvas.getContext('2d');
 
+
+// botones
+const gris = document.getElementById('gris');
+
+
+// Imagen cargada
 img.onload = function () {
     console.log("Imagen cargada");
-
     canvas.width = img.width;
     canvas.height = img.height;
     ctx.drawImage(img, 0,0); //siturar original en el canvas
-    
-    //-- Obtener la imagen del canvas en pixeles
+}
+
+// funcion para la eleccion de grises
+gris.onclick = () => {
+    var grises = 0;
     let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    //-- Obtener el array con todos los píxeles
     let data = imgData.data
+    // filtramos la imagen
+    for (var i = 0; i < data.length; i+=4) {
+        grises = (3 * data[i] + 4 * data[i+1] + data[i+2])/8
+        data[i] = grises;
+        data[i+1] = grises;
+        data[i+2] = grises;
+    }
     //-- Poner la imagen modificada en el canvas
     ctx.putImageData(imgData, 0, 0);
 }
+
+
+
