062dbf47b3bf4d756a6f7677f58cc86bc96ef5e0
lourlm <l.lozano.2017@alumnos.urjc.es>
2021-05-27 20:01:55 +0200

    cmabiando

--- P5/index.js
+++ P5/index.js
@@ -1,9 +1,9 @@
 //----- Obtener elemento de video y configurarlo
 let directo = document.getElementById("directo");
 const video1 = document.getElementById("video1");
-const b_video1 = document.getElementById("b_video1");
-const b_pantalla = document.getElementById("b_pantalla");
-const b_on = document.getElementById("b_on");
+const btn_video1 = document.getElementById("btn_video1");
+const btn_test = document.getElementById("btn_test");
+const btn_src_on = document.getElementById("btn_src_on");
 
 
 //-- Establecer las dimensiones de los vídeos
@@ -21,7 +21,7 @@ directo.poster = TEST_IMAGE_URL;
 video1.poster = TEST_IMAGE_URL;
 
 //-- Boton de FUENTES-ON
-btn_on.onclick = () => {
+btn_src_on.onclick = () => {
  
   //-- Establecer la fuente de la cámara 1
   video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
@@ -38,13 +38,13 @@ btn_on.onclick = () => {
 };
 
 //-- Botón de Test
-b_pantalla.onclick = () => {
+btn_test.onclick = () => {
     directo.poster = TEST_IMAGE_URL;
     directo.src = null;
 };
 
 //-- Botón de Selección de la cámara 1
-b_video1.onclick = () => {
+btn_video1.onclick = () => {
     directo.src = video1.src;
     directo.currentTime = video1.currentTime;
     directo.play();
