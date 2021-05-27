1a94d360d9e7e6037dd3d8d78116812fc91585ba
lourlm <l.lozano.2017@alumnos.urjc.es>
2021-05-27 22:01:12 +0200

    un video cambiaod y estilo de un boton

--- P5/index.js
+++ P5/index.js
@@ -1,9 +1,11 @@
 //----- Obtener elemento de video y configurarlo
 let directo = document.getElementById("directo");
 const video1 = document.getElementById("video1");
-const video2 = document.getElementById("video2")
+const video2 = document.getElementById("video2");
+const video3 = document.getElementById("video3");
 const btn_video1 = document.getElementById("btn_video1");
 const btn_video2 = document.getElementById("btn_video2");
+const btn_video3 = document.getElementById("btn_video3");
 const btn_test = document.getElementById("btn_test");
 const btn_src_on = document.getElementById("btn_src_on");
 
@@ -15,6 +17,8 @@ video1.width=300;
 video1.height=200;
 video2.width=300;  
 video2.height=200;
+video3.width=300;  
+video3.height=200;
 
 //-- Imagen de Test usada
 const TEST_IMAGE_URL = "monitor.jpg";
@@ -24,24 +28,28 @@ const TEST_IMAGE_URL = "monitor.jpg";
 directo.poster = TEST_IMAGE_URL;
 video1.poster = TEST_IMAGE_URL;
 video2.poster = TEST_IMAGE_URL;
+video3.poster = TEST_IMAGE_URL;
 
 //-- Boton de FUENTES-ON
 btn_src_on.onclick = () => {
  
   //-- Establecer la fuente de las camras
-  video1.src="https://github.com/LourdesLozano/Practica5/raw/main/loco_v1.mp4";
+  video1.src="https://github.com/LourdesLozano/Practica5/raw/main/pocoloco.mp4";
   video2.src="https://github.com/LourdesLozano/Practica5/raw/main/llorona.mp4";
+  video3.src="https://github.com/LourdesLozano/Practica5/raw/main/noche.mp4";
 
   //-- Reprodeucimos un vídeo, desde el comienzo
   video1.currentTime = 0;
   video1.play();
-
   video2.currentTime = 0;
   video2.play();
+  video3.currentTime = 0;
+  video3.play();
 
   //-- Y en silencio...
   video1.muted = true;
   video2.muted = true;
+  video3.muted = true;
 
   //-- En la emisión en directo ponemos la imagen de prueba
   directo.poster = TEST_IMAGE_URL;
@@ -65,6 +73,14 @@ btn_video1.onclick = () => {
 btn_video2.onclick = () => {
     directo.src = video2.src;
     directo.currentTime = video2.currentTime;
+    directo.play();
+    directo.poster=null;
+};
+
+//-- Botón de Selección de la cámara 3
+btn_video3.onclick = () => {
+    directo.src = video3.src;
+    directo.currentTime = video3.currentTime;
     directo.play();
     directo.poster=null;
 };
